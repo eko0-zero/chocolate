a6d23322616baa9d01764734bd6396c1eb33016e
eko0-zero <eko00.5177@gmail.com>
2026-05-06 09:29:35 +0200

    header base

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "./assets/svg/react.svg";
 import viteLogo from "/vite.svg";
+import arrow from "./assets/svg/arrow.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Chocolate Festival</h1>
-      <h1>Chocolate Festival</h1>
-      <h1>Chocolate Festival</h1>
+      <div className="flex gap-0.5 absolute top-10 right-15">
+        <img className="" src={arrow} alt="" />
+        <img className="-rotate-90" src={arrow} alt="" />
+        <img className="rotate-90" src={arrow} alt="" />
+        <img className="rotate-180" src={arrow} alt="" />
+      </div>
+      <div className="fixed top-9 left-15 text-sm font-bricolage">
+        <span>[</span> Menu <span>]</span>
+      </div>
+      <div className="relative">
+        <h1 className=" text-xl">Chocolate Festival</h1>
+        <h1 className=" text-xl ">Chocolate Festival</h1>
+        <h1 className=" text-xl">Chocolate Festival</h1>
+      </div>
     </>
   );
 }
